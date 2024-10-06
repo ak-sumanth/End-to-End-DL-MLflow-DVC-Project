f9bbf5b323036e2eec526cb1e2b2f69c3e8019d8
ak-sumanth <sumanthkalahari@gmail.com>
2024-10-06 06:15:33 +0000

    logger, utils and exception added

---
+++ main.py
@@ -0,0 +1,5 @@
+# from src.cnnImageClssifier import logger
+from cnnImageClssifier import logger
+
+# to check log info display this message in the terminal.
+logger.info("welcome to cnnImageClassifier!")
